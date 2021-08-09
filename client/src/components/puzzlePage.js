93036cb6ee48f58566dbd3b4f398b2cc8a168c55
Serolf777 <kookie-serolf@hotmail.com>
2021-08-09 15:31:56 -0400

    Making Heroku changes

--- client/src/components/puzzlePage.js
+++ client/src/components/puzzlePage.js
@@ -7,14 +7,14 @@ const PuzzlePage = (props) => {
       <h1>Here are some puzzles!</h1>
       <ul>
         <li><a href="/puzzle/1">Easy Puzzle!</a></li>
-        <li><a href="/puzzle/2">Another easy Puzzle!</a></li>
-        <li><a href="/puzzle/3">The last easy Puzzle!</a></li>
+        <li><a href="/puzzle/2">Another Easy Puzzle!</a></li>
+        <li><a href="/puzzle/3">The last Easy Puzzle!</a></li>
         <li><a href="/puzzle/4">Medium Puzzle!</a></li>
-        <li><a href="/puzzle/5">Another medium Puzzle!</a></li>
-        <li><a href="/puzzle/6">The last medium Puzzle!</a></li>
+        <li><a href="/puzzle/5">Another Medium Puzzle!</a></li>
+        <li><a href="/puzzle/6">The last Medium Puzzle!</a></li>
         <li><a href="/puzzle/7">Hard Puzzle!</a></li>
-        <li><a href="/puzzle/8">Another hard Puzzle!</a></li>
-        <li><a href="/puzzle/9">The last hard Puzzle!</a></li>
+        <li><a href="/puzzle/8">Another Hard Puzzle!</a></li>
+        <li><a href="/puzzle/9">The last Hard Puzzle!</a></li>
       </ul>
     </div>
   )
